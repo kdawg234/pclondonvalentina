2e0200daf406c0b4c4b356828a04ed63fe07cb23
kdawg234 <koniwinthetall@gmail.com>
2026-02-02 23:43:28 +0000

    Update script.js

--- script.js
+++ script.js
@@ -57,6 +57,7 @@ const messages = [
     "Are you sure?",
     "Really sure??",
     "Are you positive?",
+    "I'm going to tell your Mum!",
     "Pookie please...",
     "Just think about it!",
     "If you say no, I will be really sad...",
